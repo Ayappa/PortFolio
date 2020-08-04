8210af0cc1413dc335857559c0dc0c34d2fc8c59
Ayappa <ayappaganesh1995@gmail.com>
2020-08-04 00:39:54 -0400

    android projects

--- src/data.js
+++ src/data.js
@@ -52,31 +52,6 @@ export const data = {
 			},
 			{
 				id: 5,
-				name: "Product-Recomendation",
-				tools: ["Hadoop", "Spark", "Python", "Map-reduce", "Scrapper", "Flask"],
-				link: "https://github.com/Ayappa/Product-Recomendation-Hadoop",
-				description:
-					"Scraping products from websites and clean the input and store as RDDs. Present products to customer based on reviews using map reduce",
-			},
-			{
-				id: 6,
-				name: "Authentication and Authorization",
-				tools: [
-					"Android",
-					"Java",
-					"Node",
-					"API",
-					"MongoDb",
-					"Json web token",
-					"Sessions",
-				],
-				link:
-					"https://github.com/Ayappa/Authentication-using-Express-in-Android/tree/master/inClass3",
-				description:
-					"Authenticate and Authorize users in Android Application with Node API",
-			},
-			{
-				id: 7,
 				name: "Beacon Android-Shopping-app with-Stripe-Payment",
 				tools: [
 					"Android",
@@ -96,134 +71,7 @@ export const data = {
 					"Authenticate and Authorize users in Android Application to shop items with inbuild payment gateway ,handling payment token middleware in node.With Beacons , to get particular products as you near particular beacons. With shooping cart for authenticated and authorized users.",
 			},
 			{
-				id: 8,
-				name: "AirBnb Look alink hybrid App",
-				tools: [
-					"Ionic4",
-					"Angular",
-					"MongoDb",
-					"API",
-					"HTML",
-					"CSS",
-					"JavaScript",
-					"Json web token",
-					"Sessions",
-				],
-				link: "https://github.com/Ayappa/AirBnb-Like-using-Angular-and-ionic4",
-				description:
-					"Authenticate and Authorize users to book , add or explore places. With basic Crud operation for places. Node API as backend.",
-			},
-			{
-				id: 9,
-				name: "Beacon Android-Shopping-app with-BrainTree-Payment",
-				tools: [
-					"Android",
-					"Java",
-					"Node",
-					"API",
-					"MongoDb",
-					"Json web token",
-					"Sessions",
-					"Bluetooth Manager",
-					"Beacons",
-					"BrainTree Payment",
-				],
-				link: "https://github.com/Ayappa/Shooping-App-Android-and-express",
-				description:
-					"Authenticate and Authorize users in Android Application to shop items with inbuild payment gateway ,handling payment token middleware in node.With Beacons , to get particular products as you near particular beacons. With shooping cart for authenticated and authorized users.",
-			},
-			{
-				id: 10,
-				name: "ConnectionApp--Node.js",
-				tools: [
-					"Node.js",
-					"HTML",
-					"CSS",
-					"API",
-					"MongoDb",
-					"Json web token",
-					"Sessions",
-				],
-				link: "https://github.com/Ayappa/ConnectionApp--Node-js-Express-API",
-				description:
-					"Authenticate and Authorize users to create event , view or explore events around and let them know if your intrested in going , edit created events.With Node as backend.",
-			},
-			{
-				id: 11,
-				name: "Contact-Keeper FrontEnd",
-				tools: ["React", "Node.js", "HTML", "CSS", "JavaScript"],
-				link:
-					"https://github.com/Ayappa/Contact-Keeper--MERN-stack-app-using-React",
-				description:
-					"Authenticate and Authorize users to add , edit or lookup contacts . With Node as backend.",
-			},
-			{
-				id: 12,
-				name: "Contact-Keeper BackEnd",
-				tools: ["Node.js", "API", "MongoDb", "Json web token", "Sessions"],
-				link: "https://github.com/Ayappa/ContactKeeper-API-NODE-js-EXPRESS",
-				description:
-					"Authenticate and Authorize users to add , edit or lookup contacts . With Node as backend.",
-			},
-			{
-				id: 13,
-				name: "RecipeHandBook-ionic",
-				tools: ["Ionic4", "Angular", "HTML", "CSS", "JavaScript"],
-				link: "https://github.com/Ayappa/RecipeHandBook-ionic",
-				description: "Add, edit and delete recipies  on local storage ",
-			},
-			{
-				id: 14,
-				name: "GitFinder",
-				tools: ["React", "Hooks", "HTML", "CSS", "JavaScript"],
-				link: "https://github.com/Ayappa/GitFinder-ReactApp",
-				description:
-					"Search a gitProfile and view details of user and visit his git page, with Git API",
-			},
-			{
-				id: 15,
-				name: "Bluetooth Low Energy Android App",
-				tools: ["Android", "Java", "BlueTooth"],
-				link:
-					"https://github.com/Ayappa/BLE-android/blob/master/app/src/main/java/com/example/ble/MainActivity.java",
-				description:
-					"Conneect BLE devices and control the device on your app (e.g blub)",
-			},
-			{
-				id: 16,
-				name: "Hadoop-MapReduce -java",
-				tools: ["Java", "Eclipse", "Hadoop", "Map reduce"],
-				link: "https://github.com/Ayappa/Hadoop-MapReduce-in-java",
-				description:
-					"Read a huge data of file, clean the input and perform map-reduce to count word frequency",
-			},
-			{
-				id: 17,
-				name: "Emotion-Detection-with-OCR--Machine-Learning-and-Neural-Network",
-				tools: [
-					"Python",
-					"Anaconda",
-					"Machine Learning",
-					"Neural Network",
-					"Text Processing",
-					"OCR",
-					"Feature Extraction",
-				],
-				link:
-					"https://github.com/Ayappa/Emotion-Detection-with-OCR--Machine-Learning-and-Neural-Network",
-				description:
-					"React the input from image or text , and extract the text and features and predict against dataSet(emotion texts) which is cleaned(Tokenization ,Lemmatizing) in to feature set ,to predict 12 classes of emotion ",
-			},
-			{
-				id: 18,
-				name: "Android-16-small-scale-apps",
-				tools: ["Android", "Java"],
-				link: "https://github.com/Ayappa/Android-16-small-scale-apps",
-				description:
-					"Small scare app for course work , which in clude Api calls and basic android concepts for understanding",
-			},
-			{
-				id: 19,
+				id: 6,
 				name: "ChartApp-with-Uber-like-integration-Android",
 				tools: [
 					"Android",
@@ -242,39 +90,8 @@ export const data = {
 					"Authenticate and Authorize users , to create chat rooms and explore chatRooms, allow users request users a ride and allow user to select a ride from array of rides who have accepted the ride request in a particular chat room. After accepting ride allow tracking for driver and rider and finish the ride after destination arived. With node as back end for viewing the history of rides.One user can ride or drive at a given time. ",
 			},
 			{
-				id: 20,
-				name: "ChatBox",
-				tools: [
-					"Android",
-					"Java",
-					"Sessions",
-					"API",
-					"FireStore",
-					"Json Web Token",
-				],
-				link: "https://github.com/Ayappa/ChatBox",
-				description:
-					"Authenticate and Authorize users , to create chat rooms and explore chatRooms.",
-			},
-			{
-				id: 21,
-				name: "IT-Logger",
-				tools: ["React", "Hooks", "Redux", "HTML", "CSS", "JavaScript"],
-
-				link: "https://github.com/Ayappa/IT-Logger--using-REACT-REDUX",
-				description:
-					"Search , create or explore logs and store on local storage.",
-			},
-			{
-				id: 22,
-				name: "BudgetApp",
-				tools: ["Ionic4", "Angular", "HTML", "CSS", "JavaScript"],
-				link: "https://github.com/Ayappa/BudgetApp-ionic4",
-				description: "Add , edit or delete expenses on local storage",
-			},
-			{
-				id: 23,
-				name: "ChatBox",
+				id: 7,
+				name: "Business Card with Sharing over WIFI P2P",
 				tools: [
 					"Android",
 					"Java",
@@ -289,6 +106,208 @@ export const data = {
 				link: "https://github.com/Ayappa/Business-Card-through-wifiP2p-android",
 				description:
 					"Through QR code scan, we are sharing digital business cards which will have Name, phone number and email to connect and socialize.Through QR code scan, we are connecting with other users over WIFI and sharing messages and images",
+			},
+			{
+				id: 8,
+				name: "Gradng System for an Event",
+				tools: [
+					"Android",
+					"Java",
+					"Sessions",
+					"API",
+					"MongoDb",
+					"Json Web Token",
+				],
+				link: "https://github.com/Ayappa/Grade-System-for-a-Event--Android-app",
+				description:
+					"Authenticate Authorize graders and Contestant seperately.Add Contestant and send them requests.Allow grader to grade Contestant.Edit grades and calculate mean .Store History of the event.",
+			},
+			{
+				id: 9,
+				name: "Bluetooth Low Energy Android App",
+				tools: ["Android", "Java", "BlueTooth"],
+				link: "https://github.com/Ayappa/BLE-android",
+				description:
+					"Conneect BLE devices and control the device on your app (e.g blub)",
+			},
+			{
+				id: 10,
+				name: "Product-Recomendation",
+				tools: ["Hadoop", "Spark", "Python", "Map-reduce", "Scrapper", "Flask"],
+				link: "https://github.com/Ayappa/Product-Recomendation-Hadoop",
+				description:
+					"Scraping products from websites and clean the input and store as RDDs. Present products to customer based on reviews using map reduce",
+			},
+
+			{
+				id: 11,
+				name: "AirBnb Look alink hybrid App",
+				tools: [
+					"Ionic4",
+					"Angular",
+					"MongoDb",
+					"API",
+					"HTML",
+					"CSS",
+					"JavaScript",
+					"Json web token",
+					"Sessions",
+				],
+				link: "https://github.com/Ayappa/AirBnb-Like-using-Angular-and-ionic4",
+				description:
+					"Authenticate and Authorize users to book , add or explore places. With basic Crud operation for places. Node API as backend.",
+			},
+			{
+				id: 12,
+				name: "GitFinder",
+				tools: ["React", "Hooks", "HTML", "CSS", "JavaScript"],
+				link: "https://github.com/Ayappa/GitFinder-ReactApp",
+				description:
+					"Search a gitProfile and view details of user and visit his git page, with Git API",
+			},
+
+			{
+				id: 13,
+				name: "Beacon Android-Shopping-app with-BrainTree-Payment",
+				tools: [
+					"Android",
+					"Java",
+					"Node",
+					"API",
+					"MongoDb",
+					"Json web token",
+					"Sessions",
+					"Bluetooth Manager",
+					"Beacons",
+					"BrainTree Payment",
+				],
+				link: "https://github.com/Ayappa/Shooping-App-Android-and-express",
+				description:
+					"Authenticate and Authorize users in Android Application to shop items with inbuild payment gateway ,handling payment token middleware in node.With Beacons , to get particular products as you near particular beacons. With shooping cart for authenticated and authorized users.",
+			},
+
+			{
+				id: 14,
+				name: "Hadoop-MapReduce -java",
+				tools: ["Java", "Eclipse", "Hadoop", "Map reduce"],
+				link: "https://github.com/Ayappa/Hadoop-MapReduce-in-java",
+				description:
+					"Read a huge data of file, clean the input and perform map-reduce to count word frequency",
+			},
+			{
+				id: 15,
+				name: "Emotion-Detection-with-OCR--Machine-Learning-and-Neural-Network",
+				tools: [
+					"Python",
+					"Anaconda",
+					"Machine Learning",
+					"Neural Network",
+					"Text Processing",
+					"OCR",
+					"Feature Extraction",
+				],
+				link:
+					"https://github.com/Ayappa/Emotion-Detection-with-OCR--Machine-Learning-and-Neural-Network",
+				description:
+					"React the input from image or text , and extract the text and features and predict against dataSet(emotion texts) which is cleaned(Tokenization ,Lemmatizing) in to feature set ,to predict 12 classes of emotion ",
+			},
+			{
+				id: 16,
+				name: "ConnectionApp--Node.js",
+				tools: [
+					"Node.js",
+					"HTML",
+					"CSS",
+					"API",
+					"MongoDb",
+					"Json web token",
+					"Sessions",
+				],
+				link: "https://github.com/Ayappa/ConnectionApp--Node-js-Express-API",
+				description:
+					"Authenticate and Authorize users to create event , view or explore events around and let them know if your intrested in going , edit created events.With Node as backend.",
+			},
+			{
+				id: 17,
+				name: "Contact-Keeper FrontEnd",
+				tools: ["React", "Node.js", "HTML", "CSS", "JavaScript"],
+				link:
+					"https://github.com/Ayappa/Contact-Keeper--MERN-stack-app-using-React",
+				description:
+					"Authenticate and Authorize users to add , edit or lookup contacts . With Node as backend.",
+			},
+			{
+				id: 18,
+				name: "Contact-Keeper BackEnd",
+				tools: ["Node.js", "API", "MongoDb", "Json web token", "Sessions"],
+				link: "https://github.com/Ayappa/ContactKeeper-API-NODE-js-EXPRESS",
+				description:
+					"Authenticate and Authorize users to add , edit or lookup contacts . With Node as backend.",
+			},
+			{
+				id: 19,
+				name: "Authentication and Authorization",
+				tools: [
+					"Android",
+					"Java",
+					"Node",
+					"API",
+					"MongoDb",
+					"Json web token",
+					"Sessions",
+				],
+				link:
+					"https://github.com/Ayappa/Authentication-using-Express-in-Android/tree/master/inClass3",
+				description:
+					"Authenticate and Authorize users in Android Application with Node API",
+			},
+			{
+				id: 20,
+				name: "RecipeHandBook-ionic",
+				tools: ["Ionic4", "Angular", "HTML", "CSS", "JavaScript"],
+				link: "https://github.com/Ayappa/RecipeHandBook-ionic",
+				description: "Add, edit and delete recipies  on local storage ",
+			},
+
+			{
+				id: 21,
+				name: "Android-16-small-scale-apps",
+				tools: ["Android", "Java"],
+				link: "https://github.com/Ayappa/Android-16-small-scale-apps",
+				description:
+					"Small scare app for course work , which in clude Api calls and basic android concepts for understanding",
+			},
+
+			{
+				id: 22,
+				name: "ChatBox",
+				tools: [
+					"Android",
+					"Java",
+					"Sessions",
+					"API",
+					"FireStore",
+					"Json Web Token",
+				],
+				link: "https://github.com/Ayappa/ChatBox",
+				description:
+					"Authenticate and Authorize users , to create chat rooms and explore chatRooms.",
+			},
+			{
+				id: 23,
+				name: "IT-Logger",
+				tools: ["React", "Hooks", "Redux", "HTML", "CSS", "JavaScript"],
+
+				link: "https://github.com/Ayappa/IT-Logger--using-REACT-REDUX",
+				description:
+					"Search , create or explore logs and store on local storage.",
+			},
+			{
+				id: 24,
+				name: "BudgetApp",
+				tools: ["Ionic4", "Angular", "HTML", "CSS", "JavaScript"],
+				link: "https://github.com/Ayappa/BudgetApp-ionic4",
+				description: "Add , edit or delete expenses on local storage",
 			},
 		],
 	},
